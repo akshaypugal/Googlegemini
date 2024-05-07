812268aee03ea1826b6b56851ef2959347cf3e70
akshaypugal <103246625+akshaypugal@users.noreply.github.com>
2024-05-07 16:20:38 +0530

    multimodal

---
+++ multimodal.js
@@ -0,0 +1,35 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
+const dotenv = require("dotenv").config()
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType
+    },
+  };
+}
+
+async function run() {
+  // For text-and-image input (multimodal), use the gemini-pro-vision model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+  const prompt = "can describe who is more powerfull  ??";
+
+  const imageParts = [
+    fileToGenerativePart("image1.png", "image/png"),
+    fileToGenerativePart("image2.png", "image/png"),
+  ];
+
+  const result = await model.generateContent([prompt, ...imageParts]);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
